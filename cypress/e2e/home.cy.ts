513b6e273866d65d405039a744cdc69ebe0e9904
Anatoli Makarevich <makaroni4@gmail.com>
2025-02-06 18:41:20 +0100

    Fixed specs

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,11 +1,14 @@
 describe('Home Page', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/?cs=1000&cc=24&vs=1000&vc=54')
   })
 
   it('displays the correct heading', () => {
     cy.get('h1')
       .should('exist')
       .and('contain.text', 'A/B Test Calculator')
+
+    cy.contains('Statistically significant at 95% confidence level')
+      .should('exist')
   })
 })
