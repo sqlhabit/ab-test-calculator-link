ea9ed5405047ba308f2e85d0e3c1486d6f73e5cb
Anatoli Makarevich <makaroni4@gmail.com>
2025-02-06 19:07:05 +0100

    Added asset path prefix for deployed version

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const Router = process.env.NODE_ENV === 'production' ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/ab-test-calculator-link/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
