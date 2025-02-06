50dbc939e3ec47415ce7b5ea2a3145f2a5a8d9f3
Anatoli Makarevich <makaroni4@gmail.com>
2025-02-06 19:21:57 +0100

    Fix duplicated Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import { ABTestCalculator } from './components/ABTestCalculator';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <ABTestCalculator />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-50">
+      <ABTestCalculator />
+    </div>
   );
 }
 
