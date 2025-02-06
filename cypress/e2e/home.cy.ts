35add3f8a5c098e2dc8d39370222ad3f9b8da902
Anatoli Makarevich <makaroni4@gmail.com>
2025-02-06 19:58:27 +0100

    Hardcoding base url

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,9 +1,8 @@
 describe('Home Page', () => {
   beforeEach(() => {
-    const baseUrl = process.env.CYPRESS_BASE_URL || ''
-    const url = `${baseUrl}/?cs=1000&cc=24&vs=1000&vc=54`
+    const baseUrl = 'http://localhost:5173/ab-test-calculator-link/'
+    const url = `${baseUrl}?cs=1000&cc=24&vs=1000&vc=54`
 
-    cy.task("log", `--> CYPRESS_BASE_URL: ${process.env.CYPRESS_BASE_URL}`)
     cy.task("log", `--> Visiting URL: ${url}`)
 
     cy.visit(url)
